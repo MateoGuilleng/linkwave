b589e8b2983b655753f1cfe3301dee86ae73bb19
LsrkMateo <mmateoguillen@gmail.com>
2024-04-19 13:33:47 -0500

    First commit create the landing page

---
+++ src/app/home/page.jsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+export default function page() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <div className="w-full h-16 backdrop-filter backdrop-blur-xl bg-opacity-20 border-b flex items-center justify-center">
+      <div className="max-w-7xl w-full flex items-center justify-between p-4">
+        <h6 className="font-bold">Insightful</h6>
+        <ul className="flex gap-8">
+          <li>
+            <Link
+              className="hover:text-fuchsia-500 transition-colors text-xs sm:text-base"
+              href="home"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-fuchsia-500 transition-colors text-xs sm:text-base"
+              href="#about"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-fuchsia-500 transition-colors text-xs sm:text-base"
+              href="#pricing"
+            >
+              Pricing
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/libs/InfoCard.js
@@ -0,0 +1,24 @@
+import { AlarmClockOff, ArrowDownNarrowWide, ArrowUpNarrowWide } from "lucide-react";
+
+const infoCards = [
+    {
+        title: "Increased Sales",
+        bodyText: " Insightful's predictive analytics identify high-value prospects for targeted pitches, boosting conversion rates and sales by up to 20%.",
+        icon: ArrowUpNarrowWide,
+        id: 1
+    },
+    {
+        title: "No Time Wasted",
+        bodyText: "Insightful automates personalized content creation, freeing up sales reps' time for revenue-focused activities and increased productivity.",
+        icon: AlarmClockOff,
+        id: 2
+    },
+    {
+        title: "Decreased Churn",
+        bodyText: " Insightful's AI lead engagement and renewal tools reduce customer churn by up to 30% through consistent outreach and retention opportunities.",
+        icon: ArrowDownNarrowWide,
+        id: 3
+    },
+];
+
+export default infoCards;
