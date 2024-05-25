181e18560fceba4325f32a4c83a59ec6f0e0ceab
LsrkMateo <mmateoguillen@gmail.com>
2024-05-25 12:21:45 -0500

    Debug commit

--- src/app/api/testeapi/route.js
+++ src/app/api/testeapi/route.js
@@ -1,5 +1,15 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import connect from '@/utils/db';
 
-export const GET = async (request, res) => { 
-    return new NextResponse(JSON.stringify('api funciona'), { status: 200 });
-}
+export const GET = async (request, res) => {
+    try {
+        console.log('Iniciando conexión a MongoDB...');
+        await connect();
+        console.log('Conexión a MongoDB exitosa.');
+
+        return new NextResponse(JSON.stringify('API funcionaa'), { status: 200 });
+    } catch (error) {
+        console.error('Error al conectar a MongoDB:', error);
+        return new NextResponse(JSON.stringify('Error al conectar a MongoDB'), { status: 500 });
+    }
+};
